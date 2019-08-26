2e5ae0fbd736a790963f4fd04f020dd6322e8132
mubeguku <mihkelsaariste@gmail.com>
2019-08-26 15:53:06 +0300

    Update script_mobile2_wicon.js

--- script_mobile2_wicon.js
+++ script_mobile2_wicon.js
@@ -310,7 +310,9 @@
     }, 250);
 
     function numberWithCommas(x) {
-        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        if (x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
     }
     function run_all() {
 
